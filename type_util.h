650dc5feb2a7658af830cf0a666f63d6f3541cd7
yb303 <yb303@users.noreply.github.com>
2022-07-19 12:39:22 +0100

    Update type_util.h

--- type_util.h
+++ type_util.h
@@ -1,19 +1,35 @@
 #include <assert.h>
+#include <string.h> // for memcpy, memset
 #include <stdint.h>
 #include <type_traits>
 #include <tuple>
 #include <string>
+#include <new>
+#include <array>
+#include <algorithm>
 
 namespace t {
 
-/////////////
+///////////////
 
-template <class T1, class T2>
+template <class... T>
 struct concat;
 
-template <class T2, class... T1s>
-struct concat<std::tuple<T1s...>, T2> {
-    using type = std::tuple<T1s..., T2>;
+// 1 type
+template <class T>
+struct concat<T> {
+    using type = std::tuple<T>;
+};
+
+template <class... Ts>
+struct concat<std::tuple<Ts...>> {
+    using type = std::tuple<Ts...>;
+};
+
+// 2 types
+template <class T1, class T2>
+struct concat<T1, T2> {
+    using type = std::tuple<T1, T2>;
 };
 
 template <class T1, class... T2s>
@@ -21,46 +37,41 @@ struct concat<T1, std::tuple<T2s...>> {
     using type = std::tuple<T1, T2s...>;
 };
 
-template <class T1, class T2>
-using concat_t = typename concat<T1, T2>::type;
-
-/////////////
-
-template <class T1>
-struct concat2_impl;
-
-template <class... T1s>
-struct concat2_impl<std::tuple<T1s...>> {
-    template <class T2> struct add;
+template <class... T1s, class T2>
+struct concat<std::tuple<T1s...>, T2> {
+    using type = std::tuple<T1s..., T2>;
 };
 
-template <class... T1s>
-template <class... T2s>
-struct concat2_impl<std::tuple<T1s...>>::add<std::tuple<T2s...>> {
+template <class... T1s, class... T2s>
+struct concat<std::tuple<T1s...>, std::tuple<T2s...>> {
     using type = std::tuple<T1s..., T2s...>;
 };
 
-template <class T1, class T2>
-struct concat2 {
-    using type = typename concat2_impl<T1>::add<T2>::type;
+// 2+ types = 1 type + 1+ types
+template <class T1, class T2, class... Ts>
+struct concat<T1, T2, Ts...> {
+    using type = typename concat<
+                            T1,
+                            typename concat<T2, Ts...>::type
+                        >::type;
 };
 
-template <class T1, class T2>
-using concat2_t = typename concat2<T1, T2>::type;
+template <class... Ts>
+using concat_t = typename concat<Ts...>::type;
 
-/////////////
+///////////////
 
 template <int N, class _T> struct head;
 
 template <int N, class T, class... Ts>
 struct head<N, std::tuple<T, Ts...>> {
-    using type = typename concat<
+    using type = concat_t<
                     T,
                     typename head<
                         N - 1,
                         std::tuple<Ts...>
                     >::type
-                 >::type;
+                 >;
 };
 
 template <class T, class... Ts>
@@ -70,6 +81,26 @@ struct head<0, std::tuple<T, Ts...>> {
 
 template <int N, class _T>
 using head_t = typename head<N, _T>::type;
+
+/////////////
+
+template <int N, class _T, bool Stop = (N == std::tuple_size_v<_T>)> struct tail;
+
+template <int N, class... Ts>
+struct tail<N, std::tuple<Ts...>, true> {
+    using type = std::tuple<Ts...>;
+};
+
+template <int N, class T, class... Ts>
+struct tail<N, std::tuple<T, Ts...>, false> {
+    using type = typename tail<
+                        N,
+                        std::tuple<Ts...>
+                    >::type;
+};
+
+template <int N, class _T>
+using tail_t = typename tail<N, _T>::type;
 
 /////////////
 
@@ -92,6 +123,18 @@ template <int N, class _T>
 using skip_t = typename skip<N, _T>::type;
 
 /////////////
+
+template <int I, class _T> struct erase {
+    using type = concat_t<
+                    head_t<I, _T>,
+                    skip_t<I + 1, _T>
+                >;
+};
+
+template <int I, class _T>
+using erase_t = typename erase<I, _T>::type;
+
+///////////////
 
 template <int N, class _T, bool Stop = (N == 0)> struct select;
 
@@ -120,16 +163,70 @@ struct reverse<std::tuple<T>> {
     using type = std::tuple<T>;
 };
 
-template <class T0, class... Ts>
-struct reverse<std::tuple<T0, Ts...>> {
-    using type = typename concat<
+template <class T, class... Ts>
+struct reverse<std::tuple<T, Ts...>> {
+    using type = concat_t<
                     typename reverse<std::tuple<Ts...>>::type,
-                    T0
-                >::type;
+                    T
+                >;
 };
 
 template <class _T>
 using reverse_t = typename reverse<_T>::type;
+
+///////////////////
+
+template <class _T> struct reverse2;
+
+template <class T>
+struct reverse2<std::tuple<T>> {
+    using type = std::tuple<T>;
+};
+
+template <class... Ts>
+struct reverse2<std::tuple<Ts...>> {
+    static constexpr size_t n = sizeof...(Ts) / 2;
+    using type = concat_t<
+                    typename reverse2<skip_t<n, std::tuple<Ts...>>>::type,
+                    typename reverse2<head_t<n, std::tuple<Ts...>>>::type
+                >;
+};
+
+template <class _T>
+using reverse2_t = typename reverse2<_T>::type;
+
+/////////////
+
+template <template <class> class F, class _T> struct less;
+
+template <template <class> class F, class T>
+struct less<F, std::tuple<T>> {
+    static constexpr auto   value = F<T>::value;
+    static constexpr size_t index = 0;
+};
+
+template <template <class> class F, class T, class... Ts>
+struct less<F, std::tuple<T, Ts...>> {
+    using second_type = less<F, std::tuple<Ts...>>;
+    static constexpr auto lhs = F<T>::value;
+    static constexpr auto rhs = second_type::value;
+    static constexpr auto value = lhs <= rhs ? lhs : rhs;
+    static constexpr auto index = lhs <= rhs ? 0 : second_type::index + 1;
+};
+
+/////////////
+
+template <class L, class R> struct less_sz {
+    static constexpr bool value = sizeof(L) < sizeof(R);
+};
+
+template <class _T> struct min_sz2 {
+};
+template <class... Ts> struct min_sz2<std::tuple<Ts...>> {
+    static constexpr auto a1 = std::make_integer_sequence<size_t, sizeof...(Ts)>();
+    static constexpr std::array<size_t, sizeof...(Ts)> a  = { sizeof(Ts)... };
+    static constexpr size_t index = std::min_element(a.begin(), a.end()) - a.begin();
+};
 
 /////////////
 
@@ -169,16 +266,13 @@ struct pick_sort<pick_first, std::tuple<Ts...>> {
                     select_t<first_i, tup_t>,
                     typename pick_sort<
                         pick_first,
-                        concat2_t<
-                            head_t<first_i, tup_t>,
-                            skip_t<first_i + 1, tup_t>
-                        >
+                        erase_t<first_i, tup_t>
                     >::type
                 >;
 };
 
 template <class _T>
-using sort_sz_t = typename pick_sort<min_sz, _T>::type;
+using sort_sz_t = typename pick_sort<min_sz2, _T>::type;
 
 //////////////
 
@@ -187,19 +281,36 @@ void ppp() {
     printf("%s\n", __PRETTY_FUNCTION__);
 }
 
-} // namespace t
+}
 
 int main() {
     using namespace t;
 
+    static_assert(std::is_same_v<
+                    concat_t<
+                        int,
+                        bool, std::tuple<std::string>, std::tuple<>,
+                        std::tuple<double>
+                    >,
+                    std::tuple<int, bool, std::string, double>
+                >);
+
+
     static_assert(std::is_same_v<reverse_t<std::tuple<int, bool, std::string, double>>,
                                            std::tuple<double, std::string, bool, int>>);
 
+    static_assert(std::is_same_v<reverse2_t<std::tuple<int, bool, std::string, double>>,
+                                            std::tuple<double, std::string, bool, int>>);
 
     static_assert(std::is_same_v<head_t<3, std::tuple<int, bool, long, double>>,
                                            std::tuple<int, bool, long>>);
     static_assert(std::is_same_v<head_t<2, std::tuple<int, bool, long, double>>,
                                            std::tuple<int, bool>>);
+
+    static_assert(std::is_same_v<tail_t<3, std::tuple<int, bool, long, double>>,
+                                           std::tuple<     bool, long, double>>);
+    static_assert(std::is_same_v<tail_t<2, std::tuple<int, bool, long, double>>,
+                                           std::tuple<           long, double>>);
 
     static_assert(std::is_same_v<skip_t<3, std::tuple<int, bool, long, double>>,
                                            std::tuple<double>>);
@@ -209,10 +320,19 @@ int main() {
     static_assert(std::is_same_v<select_t<3, std::tuple<int, bool, long, double>>, double>);
     static_assert(std::is_same_v<select_t<2, std::tuple<int, bool, long, double>>, long>);
 
+    static_assert(min_sz_i<std::tuple<bool, int, long, double>> == 0);
     static_assert(min_sz_i<std::tuple<int, bool, long, double>> == 1);
-    static_assert(min_sz_i<std::tuple<short, int, bool, long, double>> == 2);
+    static_assert(min_sz_i<std::tuple<int, long, bool, double>> == 2);
+    static_assert(min_sz_i<std::tuple<int, long, double, bool>> == 3);
+    static_assert(min_sz_v<std::tuple<bool, int, long, double>> == 1);
+    static_assert(min_sz_v<std::tuple<int, bool, long, double>> == 1);
+    static_assert(min_sz_v<std::tuple<int, long, bool, double>> == 1);
+    static_assert(min_sz_v<std::tuple<int, long, double, bool>> == 1);
 
-    ppp<sort_sz_t<std::tuple<long, int, double, short, bool, char>>>();
+
+    ppp<sort_sz_t<std::tuple<int, bool, short, double, long>>>();
+    ppp<sort_sz_t<std::tuple<int, short, char, bool, long, double >>>();
+
 
     static_assert(std::is_same_v<sort_sz_t<std::tuple<int, bool, short, double>>,
                                            std::tuple<bool, short, int, double>>);
